d6edb35db4e1e522ce7ccfb60c77d8356d630275
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-03-19 14:15:15 -0400

    added less than response

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -13,11 +13,18 @@ public class Main {
 
 	pickedNumber= (int)(Math.random()*100);
 	System.out.println(pickedNumber);
-	System.out.println("I am thinking of a number between 1-100. Can you guess it?");
-   userNumber=keyboard.nextInt();
+	System.out.println("I am thinking of a number between 1-100. Can you guess it?");;
+        userNumber=keyboard.nextInt();
     if (userNumber<pickedNumber);{
         System.out.println("My picked number is greater than yours");
 
         }
+        userNumber=keyboard.nextInt();
+    if (userNumber>pickedNumber);
+        {
+            System.out.println("My picked number is less than yours");
+
+        }
+
     }
 }
