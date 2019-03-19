481b79e886643f4f3942dafd895500fbf9aaff7f
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-03-19 14:02:58 -0400

    Added greater than response

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,12 +1,23 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	int pickedNumber;
+	Scanner keyboard;
+	String username;
+	keyboard=new Scanner(System.in);
+	int userNumber;
 
 	pickedNumber= (int)(Math.random()*100);
 	System.out.println(pickedNumber);
-	
+	System.out.println("I am thinking of a number between 1-100. Can you guess it?");
+   userNumber=keyboard.nextInt();
+    if (userNumber<pickedNumber);{
+        System.out.println("My picked number is greater than yours");
+
+        }
     }
 }
