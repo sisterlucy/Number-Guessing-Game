1c60aecca9ed3bcf2feded553c0bc60d354b5192
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-04-04 13:00:55 -0400

    Completed project

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -11,24 +11,24 @@ public class Main {
         int userNumber;
 
         pickedNumber = (int) (Math.random() * 100);
-        System.out.println(pickedNumber);
         System.out.println("I am thinking of a number between 1-100. Can you guess it?");
         userNumber = keyboard.nextInt();
         if (userNumber == pickedNumber) {
             System.out.println("You got it right");
 
         }
-        while (userNumber != pickedNumber ) {
+        while (userNumber < pickedNumber||userNumber> pickedNumber ) {
             if (userNumber > pickedNumber) {
                 System.out.println("My number is less than yours");
                 userNumber = keyboard.nextInt();
             }
-           else if (userNumber < pickedNumber) {
+            if (userNumber < pickedNumber) {
                 System.out.println("My number is greater than yours");
-                ;
             }
-            keyboard.nextInt();
             userNumber = keyboard.nextInt();
+            if (userNumber == pickedNumber) {
+                System.out.println("You got it right");
+            }
         }
 
     }
